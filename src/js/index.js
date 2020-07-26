a3366f5c4f17898ee769a0cb25c4f0932055d245
Mateusz Kończal <mateuszkonczal96@gmail.com>
2020-07-26 18:10:50 +0200

    Moved JS scripts to src/js

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
 // uncomment the lines below to enable PWA
 // import {registerSW} from './pwa.js';
@@ -6,4 +6,72 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+const firstName = "Mateusz";
+const age = 24;
+
+// alert("Witaj, przybyszu! 👋");
+console.log(`Siema ✋, nazywam się ${firstName} i mam ${age} lata. 🚀`);
+
+// const emptyParagraph = document.querySelector(".main__description--js");
+// emptyParagraph.innerHTML = `<b>Hello!</b>`;
+
+// ZWYKŁA FUNKCJA
+function greetOld(name, age) {
+  console.log(
+    `Witaj Drogi Odwiedzający 😁. Mam na imię ${name} i mam ${age} lata.`
+  );
+}
+
+// ARROW FUNCTION
+const greet = (name, age) => {
+  console.log(
+    `Witaj Drogi Odwiedzający 😁. Mam na imię ${name} i mam ${age} lata.`
+  );
+};
+
+greetOld("Mateusz", 24);
+greet("Paweł", 24);
+
+// OBIEKT
+const person = {
+  name: "Krzysztof",
+  age: 24,
+  greet: (name, age) => {
+    console.log(
+      `Witaj Drogi Odwiedzający 😁. Mam na imię ${name} i mam ${age} lata.`
+    );
+  },
+  address: {
+    place: "Stary Jarużyn",
+    houseNumber: 41,
+    city: "Szubin",
+    postalCode: "89-200",
+  },
+};
+
+console.log(person);
+console.log(typeof person);
+console.log(`Imię: ${person.name}`);
+console.log(typeof person.name);
+console.log(`Wiek: ${person.age}`);
+console.log(typeof person.age);
+
+const myProperty = "name";
+console.log(person[myProperty]);
+console.log(person["age"]);
+
+const showMeProperty = (property) => {
+  console.log(`Twoja własność ${property} to: ${person[property]}`);
+};
+
+showMeProperty(myProperty);
+
+person.greet("Andrzej", 52);
+
+// HAMBURGER MENU
+const hamburger = document.querySelector(".hamburger--js");
+
+hamburger.addEventListener("click", () => {
+  const nav = document.querySelector(".navigation--js");
+  nav.classList.toggle("navigation--open");
+});
---
+++ src/js/script.js
@@ -0,0 +1,5 @@
+const name = "Mateusz";
+const age = 24;
+
+console.log("Hello Node.js!");
+console.log(`Siema, nazywam się ${name} i mam ${age} lata.`);
